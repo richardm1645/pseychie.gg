a73b3539ec17337bc4cd2332fdcedb074134918d
Richard Miao <richardmiao1645@gmail.com>
2023-05-28 17:55:09 -0700

    Install Tailwind

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
+import React from "react"
+
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <a href="/" className="site-title">
+    <nav className="bg-purple-800 py-4 px-8 flex items-center justify-between">
+      <a href="/" className="text-white text-xl font-bold">
         Pseychie.GG
       </a>
-      <ul>
-        <li>YouTube</li>
-        <li>Discord</li>
-        <li>Contact Us</li>
-        <li>Dark Mode</li>
+      <ul className="space-x-4 flex">
+        <li className="text-white hover:text-gray-300">
+          <a href="https://www.youtube.com/channel/UCxKlNWMOFGR4vMErkKjELuA" target="BLANK">YouTube</a>
+        </li>
+        <li className="text-white hover:text-gray-300">
+          <a href="https://discord.gg/S9vFuvqTHW">Discord</a>
+        </li>
+        <li className="text-white hover:text-gray-300"><a href="/about" target="BLANK">About</a></li>
+        <li className="text-white hover:text-gray-300">Dark Mode</li>
       </ul>
     </nav>
   )
