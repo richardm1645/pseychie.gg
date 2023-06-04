a087c456959d18b8871fc85d2761285b543b47c5
Richard Miao <richardmiao1645@gmail.com>
2023-06-04 16:39:40 -0700

    Change icon directory

--- src/components/ItemPanel.js
+++ src/components/ItemPanel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-//Mock image data - Replace when importing database
-import bb from "../assets/images/mock/Balanced_Blade_Icon.png"
 
 export default function ItemPanel({ item }) { //Replace item with props when db connects
+  const iconLocation = `/icons/${item.icon}`
+
   return (
     <div className="w-full md:w-1/3 p-6">
       <div className="bg-white rounded-lg shadow">
@@ -11,10 +11,10 @@ export default function ItemPanel({ item }) { //Replace item with props when db 
           <div className="flex flex-col">
             <div className="flex justify-between mb-3">
               <div className='flex items-center'>
-                <img src={bb} alt="Item Icon" className="w-8 h-8 mr-2" />
-                <span className="text-l font-bold max-w-xs">NAME HERE</span>
+                <img src={iconLocation} alt="Item Icon" className="w-8 h-8 mr-2" />
+                <span className="text-l font-bold max-w-xs">{item.name}</span>
               </div>             
-              <span className="text-xl font-geologica font-bold">{item.grade}</span>
+              <span className="text-xl font-geologica font-bold">{item.rating}</span>
             </div>
             <p className="text-sm">{item.description}</p>
           </div>
