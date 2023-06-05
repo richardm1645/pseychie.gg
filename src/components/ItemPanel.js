1ca242811a8bb08c47f520ce6f68095f78400c5c
Richard Miao <richardmiao1645@gmail.com>
2023-06-04 17:19:42 -0700

    Add color backgrounds

--- src/components/ItemPanel.js
+++ src/components/ItemPanel.js
@@ -6,12 +6,15 @@ export default function ItemPanel({ item }) { //Replace item with props when db 
 
   return (
     <div className="w-full md:w-1/3 p-6">
-      <div className="bg-white rounded-lg shadow">
+      <div className="bg-white rounded-lg shadow hover:shadow-outline">
         <div className="p-4 h-48">
           <div className="flex flex-col">
             <div className="flex justify-between mb-3">
               <div className='flex items-center'>
-                <img src={iconLocation} alt="Item Icon" className="w-8 h-8 mr-2" />
+                <div className="relative w-12 h-12 mr-4"> 
+                  <img src='colors/BS.png' alt="My Image" className="max-w-10 drop-shadow-md" />               
+                  <img src={iconLocation} alt="Item Icon" className="absolute top-1 left-1 w-10 drop-shadow-md shadow-black opacity-90" />              
+                </div>
                 <span className="text-l font-bold max-w-xs">{item.name}</span>
               </div>             
               <span className="text-xl font-geologica font-bold">{item.rating}</span>
