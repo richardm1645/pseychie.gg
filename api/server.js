cc2d49b64a1e14a169e837cdfd0b92da02f3b3d0
Richard Miao <richardmiao1645@gmail.com>
2023-06-27 16:33:58 -0700

    Create API endpoint for fetching single item

--- api/server.js
+++ api/server.js
@@ -24,6 +24,30 @@ app.get('/api/data', async (req, res) => {
   }
 });
 
+// Define route to handle item description API call
+app.get('/api/item/:id', async (req, res) => {
+  const itemId = req.params.id;
+
+  try {
+    const db = await connect();
+    const collection = db.collection('gear');
+    const result = await collection.findOne({ url: itemId })
+
+    if (result) {
+      // Return the item object as a response
+      res.json(result);
+    } else {
+      res.status(404).json({ error: 'Item not found' });
+    }
+
+  } catch (error) {
+    console.error('Error performing query:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+  
+});
+
+
 // Start the API server
 app.listen(5000, () => {
   console.log('API server listening on port 5000');
